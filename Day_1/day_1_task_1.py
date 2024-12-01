785d2922a0be4b9058d1bcfd8a3fae9dbcb9c1d4
Aidan Sabin <psyas19@nottingham.ac.uk>
2024-12-01 21:38:04 +0000

    complete day 1 task 1 (success)

---
+++ Day_1/day_1_task_1.py
@@ -0,0 +1,25 @@
+# Read the file
+with open('Day_1/input.py', 'r') as file:
+    lines = file.readlines()
+
+# Store the values
+first_values = []
+second_values = []
+
+# Extract values from each line
+for line in lines:
+    first, second = map(int, line.split())
+    first_values.append(first)
+    second_values.append(second)
+
+# Sort into ascending order
+first_values.sort()
+second_values.sort()
+
+result = 0
+for i in range(0, len(first_values)):
+    first = first_values[i]
+    second = second_values[i]
+    result += abs(first - second)
+
+print(result)
